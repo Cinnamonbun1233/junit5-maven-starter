a5dc604a592ca74bb131affc41c0d37bfeb88f5d
dima <cinnamonbun1233@yandex.ru>
2023-07-18 18:33:52 +0300

    feat: добавил тесты

--- src/test/java/com/example/junit/TestLauncher.java
+++ src/test/java/com/example/junit/TestLauncher.java
@@ -3,6 +3,7 @@ package com.example.junit;
 import org.junit.platform.engine.discovery.DiscoverySelectors;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TagFilter;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
@@ -22,6 +23,10 @@ public class TestLauncher {
                 .request()
 //                .selectors(DiscoverySelectors.selectClass(UserServiceTest.class))
                 .selectors(DiscoverySelectors.selectPackage("com.example.junit.service"))
+                .filters(
+//                        TagFilter.includeTags("login")
+                        TagFilter.excludeTags("login")
+                )
 //                .listeners()
                 .build();
         launcher.execute(request, summaryGeneratingListener);
--- src/test/java/com/example/junit/service/UserServiceTest.java
+++ src/test/java/com/example/junit/service/UserServiceTest.java
@@ -3,14 +3,32 @@ package com.example.junit.service;
 import com.example.junit.dto.User;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("fast")
+
+// Рандомая сортировка
+//@TestMethodOrder(MethodOrderer.Random.class)
+
+// Сортировка по @Order
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+// Сортировка по алфавитному порядку
+//@TestMethodOrder(MethodOrderer.MethodName.class)
+
+// Сорттровка по @DisplayName
+//@TestMethodOrder(MethodOrderer.DisplayName.class)
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class UserServiceTest {
@@ -29,6 +47,8 @@ public class UserServiceTest {
         userService = new UserService();
     }
 
+    @Order(1)
+//    @DisplayName("AAA")
     @Test
     void usersEmptyIfNoUserAdded() {
         //System.out.println("Test 1: " + this);
@@ -55,20 +75,8 @@ public class UserServiceTest {
         //assertEquals(2, users.size());
     }
 
-    @Test
-    void loginSuccessIfUserExists() {
-        userService.add(IVAN);
-        Optional<User> maybeUser = userService.login(IVAN.getUserName(), IVAN.getPassword());
 
-        //assertj
-        assertThat(maybeUser).isPresent();
-        maybeUser.ifPresent(user -> assertThat(user).isEqualTo(IVAN));
-
-        //junit
-        //assertTrue(maybeUser.isPresent());
-        //maybeUser.ifPresent(user -> assertEquals(IVAN, user));
-    }
-
+    @Order(2)
     @Test
     void throwExceptionIfUsernameOrPasswordIsNull() {
 //        try {
@@ -105,24 +113,6 @@ public class UserServiceTest {
         //MatcherAssert.assertThat(users, IsMapContaining.hasKey(IVAN.getId()));
     }
 
-    @Test
-    void loginFailIfPasswordIsNotCorrect() {
-        userService.add(IVAN);
-        Optional<User> maybeUser = userService.login(IVAN.getPassword(), "dummy");
-
-        //junit
-        assertTrue(maybeUser.isEmpty());
-    }
-
-    @Test
-    void loginFailIfUserDoesNotExist() {
-        userService.add(IVAN);
-        Optional<User> maybeUser = userService.login("dummy", IVAN.getPassword());
-
-        //junit
-        assertTrue(maybeUser.isEmpty());
-    }
-
     @AfterEach
     void after() {
         //System.out.println("After each: " + this);
@@ -131,5 +121,89 @@ public class UserServiceTest {
     @AfterAll
     static void afterAll() {
         //System.out.println("After all: ");
+    }
+
+    @Nested
+    @DisplayName("Test login")
+    @Tag("login")
+    class LoginTest {
+        @Test
+        @Tag("login")
+        void loginFailIfPasswordIsNotCorrect() {
+            userService.add(IVAN);
+            Optional<User> maybeUser = userService.login(IVAN.getPassword(), "dummy");
+
+            //junit
+            assertTrue(maybeUser.isEmpty());
+        }
+
+        @Test
+        @Tag("login")
+        void loginFailIfUserDoesNotExist() {
+            userService.add(IVAN);
+            Optional<User> maybeUser = userService.login("dummy", IVAN.getPassword());
+
+            //junit
+            assertTrue(maybeUser.isEmpty());
+        }
+
+        @Test
+        @Tag("login")
+        void loginSuccessIfUserExists() {
+            userService.add(IVAN);
+            Optional<User> maybeUser = userService.login(IVAN.getUserName(), IVAN.getPassword());
+
+            //assertj
+            assertThat(maybeUser).isPresent();
+            maybeUser.ifPresent(user -> assertThat(user).isEqualTo(IVAN));
+
+            //junit
+            //assertTrue(maybeUser.isPresent());
+            //maybeUser.ifPresent(user -> assertEquals(IVAN, user));
+        }
+
+        @ParameterizedTest(name = "Тесты на логин {index}")
+
+//        @ArgumentsSource()
+
+//        @NullSource
+
+//        @EmptySource
+
+//        @NullAndEmptySource
+
+//        @ValueSource(strings = {
+//                "Ivan", "Petr"
+//        })
+
+//        @EnumSource
+
+        @MethodSource("com.example.junit.service.UserServiceTest#getArgumentsForLoginTest")
+
+//        @CsvFileSource(resources = "/login-test-data.csv", delimiter = ',', numLinesToSkip = 1)
+
+
+//        @CsvSource({
+//                "Ivan,123",
+//                "Petr, 456"
+//        })
+//        void loginParametrizedTest(String username, String password) {
+
+            // для @MethodSource
+        void loginParametrizedTest(String username, String password, Optional<User> user) {
+            userService.add(IVAN, PETR);
+
+            Optional<User> maybeUser = userService.login(username, password);
+            assertThat(maybeUser).isEqualTo(user);
+        }
+    }
+
+    static Stream<Arguments> getArgumentsForLoginTest() {
+        return Stream.of(
+                Arguments.of("Ivan", "123", Optional.of(IVAN)),
+                Arguments.of("Petr", "456", Optional.of(PETR)),
+                Arguments.of("Petr", "dummy", Optional.empty()),
+                Arguments.of("dummy", "123", Optional.empty())
+        );
     }
 }
